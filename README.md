02f9cc23649cba728cc0ddce23da6ebdd7167ab6
Patricio <pcifrey501@g.educaand.es>
2024-02-11 14:04:58 +0100

    is working, miracle

---
+++ README.md
@@ -0,0 +1,11 @@
+# tinyquestions - Backend con Laravel
+____
+
+0 - Diagrama entidad relación - Paso a tablas -Normalización 
+=
+---
+
+![diagram for my database](/00-images-readme/diagram.png)
+![diagram for my database](/00-images-readme/tables-norm.JPG)
+
+
---
+++ db-data/01-schema.sql
@@ -0,0 +1,76 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS followers;
+DROP TABLE IF EXISTS matches;
+DROP TABLE IF EXISTS likes;
+DROP TABLE IF EXISTS quizzes;
+DROP TABLE IF EXISTS custom_quizzes;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS quiz_has_questions;
+
+CREATE TABLE users (
+                       id_user BIGINT PRIMARY KEY AUTO_INCREMENT,
+                       username VARCHAR(30),
+                       email VARCHAR(100),
+                       password VARCHAR(40),
+                       points INT,
+                       quizzes_resolved INT
+);
+
+CREATE TABLE quizzes (
+                         id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                         n_questions INT,
+                         type ENUM('custom', 'quick', 'mirror', 'exploding', 'zen'),
+                         clock ENUM('yes', 'no'),
+                         time INT CHECK (time >= 5)
+    );
+
+CREATE TABLE questions (
+                           id_question BIGINT PRIMARY KEY AUTO_INCREMENT,
+                           title VARCHAR(60),
+                           option_a VARCHAR(30),
+                           option_b VARCHAR(30),
+                           option_c VARCHAR(30),
+                           correct_option VARCHAR(30),
+                           points INT
+);
+
+CREATE TABLE custom_quizzes (
+                                id_quiz BIGINT PRIMARY KEY,
+                                clock ENUM('yes', 'no'),
+                                time INT CHECK (time >= 5),
+    fk_id_user BIGINT,
+    FOREIGN KEY (fk_id_user) REFERENCES users(id_user)
+);
+
+CREATE TABLE quiz_has_questions (
+                                    id_quiz BIGINT,
+                                    id_question BIGINT,
+                                    PRIMARY KEY (id_quiz, id_question),
+                                    FOREIGN KEY (id_quiz) REFERENCES quizzes(id),
+                                    FOREIGN KEY (id_question) REFERENCES questions(id_question)
+);
+
+CREATE TABLE followers (
+                           id_user_follow BIGINT,
+                           id_user_followed BIGINT,
+                           PRIMARY KEY (id_user_follow, id_user_followed),
+                           FOREIGN KEY (id_user_follow) REFERENCES users(id_user),
+                           FOREIGN KEY (id_user_followed) REFERENCES users(id_user)
+);
+
+CREATE TABLE matches (
+                         id_user BIGINT,
+                         id_quiz BIGINT,
+                         right_answers INT,
+                         PRIMARY KEY (id_user, id_quiz),
+                         FOREIGN KEY (id_user) REFERENCES users(id_user),
+                         FOREIGN KEY (id_quiz) REFERENCES quizzes(id)
+);
+
+CREATE TABLE likes (
+                       id_user BIGINT,
+                       id_quiz BIGINT,
+                       PRIMARY KEY (id_user, id_quiz),
+                       FOREIGN KEY (id_user) REFERENCES users(id_user),
+                       FOREIGN KEY (id_quiz) REFERENCES quizzes(id)
+);
