a1627ee71c078e82d9b072af8853723115f7b470
Patricio <pcifrey501@g.educaand.es>
2024-02-12 17:22:10 +0100

    crud implemented

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+use App\Models\Question;
+use Illuminate\Http\Request;
+
+
+class QuestionController extends Controller
+{
+
+    public function allQuestions(): \Illuminate\Http\JsonResponse
+    {
+        $results = Question::all();
+        return response()->json($results);
+    }
+
+    public function getRandomQuestions($numberQuestionsNeeded): \Illuminate\Http\JsonResponse
+    {
+        //$results = Question::take(25)->get();
+        $totalQuestions = $this->allQuestions();
+        $arrayOfAllQuestions = json_decode($totalQuestions->getContent(), true);;
+
+
+        shuffle($arrayOfAllQuestions);
+
+        $questionsChoosed = [];
+        $counter = 0;
+        while (count($questionsChoosed)<$numberQuestionsNeeded){
+
+            $questionsChoosed[]=$arrayOfAllQuestions[$counter];
+            $counter++;
+        }
+
+        return response()-> json($questionsChoosed);
+
+    }
+
+    public function createQuestion(Request $request)
+    {
+        $question = Question::create($request->all());
+        return response()->json($question, 201);
+    }
+
+
+    public function getById($id)
+    {
+        $question = Question::findOrFail($id);
+        return response()->json($question);
+    }
+
+
+    public function updateQuestion(Request $request, $id)
+    {
+        $question = Question::findOrFail($id);
+
+        $request->validate(Question::rulesForUsers());
+
+        $question->fill($request->only([
+            'title',
+            'option_a',
+            'option_b',
+            'option_c',
+            'correct_option',
+        ]));
+
+        return response()->json($question);
+
+    }
+
+    public function deleteQuestion($id)
+    {
+        $question = Question::findOrFail($id);
+        $question->delete();
+        return response()->json(null, 204);
+    }
+
+
+
+}
+
