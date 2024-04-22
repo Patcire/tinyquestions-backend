9f7a938e070d69c7da5b10d9c94ae661fcfe1dff
Patricio <pcifrey501@g.educaand.es>
2024-04-22 18:20:35 +0200

    sql code and values working

--- db-data/01-schema.sql
+++ db-data/01-schema.sql
@@ -4,72 +4,91 @@ DROP TABLE IF EXISTS matches;
 DROP TABLE IF EXISTS likes;
 DROP TABLE IF EXISTS custom_quizzes;
 DROP TABLE IF EXISTS custom_questions;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS multiplayer_matches;
+DROP TABLE IF EXISTS singleplayer_matches;
+DROP TABLE IF EXISTS random_quiz_has_random_question;
+
 
 CREATE TABLE `users` (
-                        `id_user` BIGINT AUTO_INCREMENT PRIMARY KEY,
-                        `username` VARCHAR(30) UNIQUE NOT NULL,
-                        `email` VARCHAR(100) NOT NULL,
-                        `password` VARCHAR(200) NOT NULL,
-                        `points` INT DEFAULT 0,
-                        `quizzes_resolved` INT DEFAULT 0
+            `id_user` BIGINT AUTO_INCREMENT PRIMARY KEY,
+            `username` VARCHAR(30) UNIQUE NOT NULL,
+            `email` VARCHAR(100) NOT NULL,
+            `password` VARCHAR(50) NOT NULL,
+            `points` INT DEFAULT 0,
+            `quizzes_resolved` INT DEFAULT 0
 );
 
 CREATE TABLE `followers` (
-                            `id_user_follow` BIGINT,
-                            `id_user_followed` BIGINT,
-                            PRIMARY KEY (`id_user_follow`, `id_user_followed`),
-                            FOREIGN KEY (`id_user_follow`) REFERENCES `users`(`id_user`) ON DELETE CASCADE,
-                            FOREIGN KEY (`id_user_followed`) REFERENCES `users`(`id_user`) ON DELETE CASCADE
+            `id_user_follow` BIGINT,
+            `id_user_followed` BIGINT,
+            PRIMARY KEY (`id_user_follow`, `id_user_followed`),
+            FOREIGN KEY (`id_user_follow`) REFERENCES `users`(`id_user`) ON DELETE CASCADE,
+            FOREIGN KEY (`id_user_followed`) REFERENCES `users`(`id_user`) ON DELETE CASCADE
+);
+
+
+CREATE TABLE `quizzes` (
+          `id_quiz` BIGINT AUTO_INCREMENT PRIMARY KEY,
+          `quiz_name` VARCHAR(40) NOT NULL,
+          `number_questions` INT NOT NULL,
+          `clock` BOOLEAN NOT NULL,
+          `time` INT CHECK (time >= 5),
+          `fk_id_user` BIGINT,
+          FOREIGN KEY (`fk_id_user`) REFERENCES `users`(`id_user`) ON DELETE CASCADE
 );
 
 CREATE TABLE `custom_quizzes` (
-                          `id_quiz` BIGINT AUTO_INCREMENT PRIMARY KEY,
-                          `quiz_name` VARCHAR(40) NOT NULL,
-                          `n_questions` INT NOT NULL,
-                          `clock` INT NOT NULL,
-                          `time` INT CHECK (time >= 5),
-                          `fk_id_user` BIGINT,
-                          FOREIGN KEY (`fk_id_user`) REFERENCES `users`(`id_user`) ON DELETE CASCADE
+           `id_quiz` BIGINT PRIMARY KEY,
+           `fk_id_user` BIGINT,
+           FOREIGN KEY (id_quiz) REFERENCES quizzes(id_quiz),
+           FOREIGN KEY (`fk_id_user`) REFERENCES `users`(`id_user`) ON DELETE CASCADE
 );
 
-
 CREATE TABLE `matches` (
-                         `pk_match` BIGINT AUTO_INCREMENT PRIMARY KEY,
-                          `match_identifier` BIGINT AUTO_INCREMENT,
-                         `fk_id_user` BIGINT,
-                         `fk_id_quiz` BIGINT,
-                         `number_right_answers` INT,
-                          `date` TIMESTAMP,
-                         FOREIGN KEY (`fk_id_user`) REFERENCES `users`(`id_user`),
-                         FOREIGN KEY (`fk_id_custom_quiz`) REFERENCES `custom_quizzes`(`id_quiz`) ON DELETE CASCADE
-                         UNIQUE KEY `unique_match_user_quiz` (`fk_id_user`, `fk_id_quiz`, `id_match`)
+
+       `id_match` BIGINT AUTO_INCREMENT PRIMARY KEY ,
+       `type` VARCHAR(20),
+       `fk_id_quiz` BIGINT,
+       FOREIGN KEY (`fk_id_quiz`) REFERENCES `quizzes`(`id_quiz`),
+       CONSTRAINT type_check CHECK (type IN ('single', 'multi'))
+
+);
+
+CREATE TABLE `multiplayer_matches` (
+
+       `id_match` BIGINT PRIMARY KEY ,
+       `winner` VARCHAR(30),
+       `number_players` INT CHECK ( number_players <= 8),
+       FOREIGN KEY (`id_match`) REFERENCES `matches` (`id_match`)
+);
+
+CREATE TABLE `singleplayer_matches` (
+
+    `id_match` BIGINT PRIMARY KEY ,
+    `mode` VARCHAR(30),
+    CONSTRAINT mode_check CHECK (mode IN ('quick', 'mirror', 'explod')),
+    FOREIGN KEY (`id_match`) REFERENCES `matches` (`id_match`)
 );
 
 CREATE TABLE `likes` (
-                         `id_like` BIGINT AUTO_INCREMENT PRIMARY KEY,
-                         `fk_id_user` BIGINT,
-                         `fk_id_quiz` BIGINT,
-                         FOREIGN KEY (`fk_id_user`) REFERENCES `users`(`id_user`) ON DELETE CASCADE,
-                         FOREIGN KEY (`fk_id_quiz`) REFERENCES `custom_quizzes`(`id_quiz`) ON DELETE CASCADE,
-                         UNIQUE KEY `unique_like_user_quiz` (`fk_id_user`, `fk_id_quiz`)
+     `id_like` BIGINT AUTO_INCREMENT PRIMARY KEY,
+     `fk_id_user` BIGINT,
+     `fk_id_quiz` BIGINT,
+     FOREIGN KEY (`fk_id_user`) REFERENCES `users`(`id_user`) ON DELETE CASCADE,
+     FOREIGN KEY (`fk_id_quiz`) REFERENCES `custom_quizzes`(`id_quiz`) ON DELETE CASCADE,
+     UNIQUE KEY `unique_like_user_quiz` (`fk_id_user`, `fk_id_quiz`)
 );
 
+CREATE TABLE `users_plays_matches` (
 
-CREATE TABLE `custom_questions` (
-                                    `id_question` BIGINT AUTO_INCREMENT PRIMARY KEY,
-                                    `title` VARCHAR(60) NOT NULL,
-                                    `option_a` VARCHAR(30) NOT NULL,
-                                    `option_b` VARCHAR(30) NOT NULL,
-                                    `option_c` VARCHAR(30) NOT NULL,
-                                    `correct_option` VARCHAR(30) NOT NULL,
-                                    `points` INT,
-                                    `fk_id_quiz` BIGINT,
-                                    FOREIGN KEY (`fk_id_quiz`) REFERENCES `custom_quizzes`(`id_quiz`) ON DELETE CASCADE
+       `id_user` BIGINT,
+       `id_match` BIGINT,
+       PRIMARY KEY (id_match, id_user),
+       FOREIGN KEY (id_match) REFERENCES matches(id_match),
+       FOREIGN KEY (id_user) REFERENCES users(id_user)
+
 );
-
--- QUESTIONS FROM APP
-
-DROP TABLE IF EXISTS questions;
 
 CREATE TABLE `questions`
 (
@@ -79,5 +98,33 @@ CREATE TABLE `questions`
     `option_b`       VARCHAR(30) NOT NULL,
     `option_c`       VARCHAR(30) NOT NULL,
     `correct_option` VARCHAR(30) NOT NULL,
-    `points`         INT
+    `points`         INT,
+    type VARCHAR(20),
+    CONSTRAINT quest_type_check CHECK (type IN ('custom', 'random')),
+    CONSTRAINT correct_option_check CHECK (
+        correct_option IN (option_a, option_b, option_c))
 );
+
+
+CREATE TABLE `custom_questions` (
+    `id_question` BIGINT PRIMARY KEY,
+    `fk_id_quiz` BIGINT,
+    FOREIGN KEY (`id_question`) REFERENCES `questions`(`id_question`) ON DELETE CASCADE,
+    FOREIGN KEY (`fk_id_quiz`) REFERENCES `custom_quizzes`(`id_quiz`) ON DELETE CASCADE
+
+);
+
+
+
+CREATE TABLE `random_quiz_has_random_question` (
+        `id_quiz` BIGINT PRIMARY KEY,
+        `id_question` BIGINT,
+        FOREIGN KEY (`id_quiz`) REFERENCES `quizzes`(`id_quiz`) ON DELETE CASCADE,
+        FOREIGN KEY (`id_question`) REFERENCES `questions` (`id_question`) ON DELETE CASCADE
+
+);
+
+
+
+
+
--- db-data/02-data.sql
+++ db-data/02-data.sql
@@ -1,130 +1,135 @@
-INSERT INTO questions (id_question, title, option_a, option_b, option_c, correct_option, points) VALUES
-(1, 'Director of "Lost in Translation"?', 'Sofia Coppola', 'Quentin Tarantino', 'Christopher Nolan', 'Sofia Coppola', 10),
-(2, '2014 Best Picture indie?', 'Moonlight', 'Boyhood', 'La La Land', 'Moonlight', 10),
-(3, 'Greta Gerwig''s debut?', 'Lady Bird', 'Little Women', 'Frances Ha', 'Lady Bird', 10),
-(4, 'Director of "The Hurt Locker"?', 'Kathryn Bigelow', 'Ava DuVernay', 'Patty Jenkins', 'Kathryn Bigelow', 10),
-(5, 'Band behind "Vespertine"?', 'Björk', 'Radiohead', 'Arcade Fire', 'Björk', 10),
-(6, 'Lorde''s real name?', 'Ella Marija Lani Yelich', 'Stefani Joanne Angelina', 'Robyn Rihanna Fenty', 'Ella Marija Lani Yelich', 10),
-(7, 'The National''s lead vocalist?', 'Matt Berninger', 'Thom Yorke', 'Brandon Flowers', 'Matt Berninger', 10),
-(8, 'Caroline Polachek most viral song?', 'Hurting My Feelings', 'Door', 'sunset', 'Hurting My Feelings', 10),
-(9, '1988 Summer Olympics host?', 'South Korea', 'United States', 'Australia', 'South Korea', 10),
-(10, '2006 FIFA World Cup winner?', 'Italy', 'Brazil', 'Germany', 'Italy', 10),
-(11, 'Sport associated with "slam dunk"?', 'Basketball', 'Football', 'Tennis', 'Basketball', 10),
-(12, '"The Greatest" in boxing?', 'Muhammad Ali', 'Mike Tyson', 'Floyd Mayweather Jr.', 'Muhammad Ali', 10),
-(13, 'First female UK Prime Minister?', 'Margaret Thatcher', 'Theresa May', 'Angela Merkel', 'Margaret Thatcher', 10),
-(14, 'Year of the Berlin Wall fall?', '1989', '1991', '1987', '1989', 10),
-(15, 'Longest US president term?', 'Franklin D. Roosevelt', 'George Washington', 'Abraham Lincoln', 'Franklin D. Roosevelt', 10),
-(16, 'USSR leader during Cuban Missile Crisis?', 'Nikita Khrushchev', 'Leonid Brezhnev', 'Joseph Stalin', 'Nikita Khrushchev', 10),
-(17, 'Current German Chancellor?', 'Olaf Scholz', 'Angela Merkel', 'Emmanuel Macron', 'Olaf Scholz', 10),
-(18, 'First female Brazilian President?', 'Dilma Rousseff', 'Cristina Fernandez de Kirchner', 'Michelle Bachelet', 'Dilma Rousseff', 10),
-(19, '"Libertarianism" political ideology?', 'Ltd government intervention', 'Strong government control', 'Totalitarianism', 'Lmtd government intervention', 10),
-(20, '44th US President?', 'Barack Obama', 'Donald Trump', 'George W. Bush', 'Barack Obama', 10),
-(21, 'Actor: Tony Stark in MCU?', 'Robert Downey Jr.', 'Chris Hemsworth', 'Chris Evans', 'Robert Downey Jr.', 10),
-(22, '"Queen of Pop"?', 'Madonna', 'Beyoncé', 'Rihanna', 'Madonna', 10),
-(23, 'Oscar winner for "La La Land"?', 'Emma Stone', 'Natalie Portman', 'Jennifer Lawrence', 'Emma Stone', 10),
-(24, 'Singer-songwriter of "25" (2015)?', 'Adele', 'Taylor Swift', 'Beyoncé', 'Adele', 10),
-(25, 'Water chemical symbol?', 'H2O', 'CO2', 'NaCl', 'H2O', 10),
-(26, '"Red Planet"?', 'Mars', 'Venus', 'Jupiter', 'Mars', 10),
-(27, 'Australia capital city?', 'Canberra', 'Sydney', 'Melbourne', 'Canberra', 10),
-(28, '"To Kill a Mockingbird" author?', 'Harper Lee', 'John Steinbeck', 'Ernest Hemingway', 'Harper Lee', 10);
+INSERT INTO users (username, email, password, points, quizzes_resolved)
+VALUES  ('pepepepe', 'pepe@gmail.com', 'pepepepe', 100, 3),
+        ('usuario2', 'usuario2@example.com', 'password2', 50, 1),
+        ('usuario3', 'usuario3@example.com', 'password3', 75, 2),
+        ('usuario4', 'usuario4@example.com', 'password4', 200, 5),
+        ('usuario5', 'usuario5@example.com', 'password5', 150, 4);
 
--- more questions
-
-# + questions
-INSERT INTO questions (id_question, title, option_a, option_b, option_c, correct_option, points) VALUES
-(86, 'Painter known for "The Two Fridas"', 'Georgia OKeeffe', 'Frida Kahlo', 'Tamara de Lempicka', 'Tamara de Lempicka', 10),
-(87, 'Singer known for "Rehab"', 'Adele', 'Amy Winehouse', 'Taylor Swift', 'Amy Winehouse', 10),
-(88, 'Director of "The Hurt Locker"', 'Kathryn Bigelow', 'Ava DuVernay', 'Patty Jenkins', 'Kathryn Bigelow', 10),
-(89, 'Singer-songwriter known for "Jolene"', 'Dolly Parton', 'Carole King', 'Joan Baez', 'Dolly Parton', 10),
-(90, 'Artist known for "The Dinner Party"', 'Louise Bourgeois', 'Yayoi Kusama', 'Judy Chicago', 'Yayoi Kusama', 10),
-(91, 'Actress known for "Breakfast at Tiffanys"', 'Audrey Hepburn', 'Marilyn Monroe', 'Grace Kelly', 'Audrey Hepburn', 10),
-(92, 'Painter known for "Ophelia"', 'Georgia OKeeffe', 'Frida Kahlo', 'John Singer Sargent', 'Frida Kahlo', 10),
-(93, 'Singer known for "Rolling in the Deep"', 'Adele', 'Amy Winehouse', 'Taylor Swift', 'Adele', 10),
-(94, 'Director of "Carol"', 'Todd Haynes', 'Woody Allen', 'Francis Ford Coppola', 'Todd Haynes', 10),
-(95, 'Singer-songwriter known for "Wuthering Heights"', 'Bjork', 'Kate Bush', 'Sia', 'Kate Bush', 10),
-(96, 'Artist known for "My Bed"', 'Yayoi Kusama', 'Tracey Emin', 'Banksy', 'Tracey Emin', 10),
-(97, 'Actress known for "Monster"', 'Charlize Theron', 'Meryl Streep', 'Angelina Jolie', 'Charlize Theron', 10),
-(98, 'Painter known for "The Persistence of Memory"', 'Georgia OKeeffe', 'Frida Kahlo', 'Salvador Dali', 'Salvador Dali', 10),
-(99, 'Singer known for "Big Yellow Taxi"', 'Joni Mitchell', 'Carole King', 'Janis Joplin', 'Joni Mitchell', 10),
-(100, 'Director of "American Psycho"', 'Kathryn Bigelow', 'Sofia Coppola', 'Mary Harron', 'Mary Harron', 10),
-(101, 'Singer-songwriter known for "Pagan Poetry"', 'Bjork', 'Kate Bush', 'Sia', 'Bjork', 10),
-(102, 'Artist known for "Love Is What You Want"', 'Yayoi Kusama', 'Tracey Emin', 'Agnes Martin', 'Tracey Emin', 10),
-(103, 'Actress of "Mad Max: Fury Road"', 'Charlize Theron', 'Meryl Streep', 'Angelina Jolie', 'Charlize Theron', 10),
-(104, 'Artist of Self Portrait with Thorn Necklace and Hummingbird', 'Georgia OKeeffe', 'Frida Kahlo', 'Tamara de Lempicka', 'Frida Kahlo', 10),
-(105, 'Singer known for "River"', 'Joni Mitchell', 'Carole King', 'Janis Joplin', 'Joni Mitchell', 10),
-(106, 'Novel known for "Strangers on a Train"', 'Rebecca', 'Psycho', 'Strangers on a Train', 'Strangers on a Train', 10),
-(107, 'Singer-songwriter known for "Running Up That Hill"', 'Bjork', 'Kate Bush', 'Sia', 'Kate Bush', 10),
-(108, 'Artist known for "Spiral Jetty"', 'Yayoi Kusama', 'Tracey Emin', 'Agnes Martin', 'Agnes Martin', 10),
-(109, 'Actress known for "The Devil Wears Prada"', 'Anne Hathaway', 'Meryl Streep', 'Julia Roberts', 'Meryl Streep', 10),
-(110, 'Beach house bloom album year"', '2012', '2008', '2019', '2012', 10),
-(111, 'Singer known for "From the Choirgirl Hotel"', 'Tori Amos', 'PJ Harvey', 'Alanis Morissette', 'Tori Amos', 10),
-(112, 'Director of "American Psycho"', 'Kathryn Bigelow', 'Sofia Coppola', 'Mary Harron', 'Mary Harron', 10),
-(113, 'Singer-songwriter known for "Army of Me"', 'Bjork', 'Kate Bush', 'Sia', 'Bjork', 10),
-(114, 'Artist known for "Infinity Mirror Room"', 'Yayoi Kusama', 'Tracey Emin', 'Agnes Martin', 'Yayoi Kusama', 10),
-(115, 'Actress known for "Promising Young Woman"', 'Carey Mulligan', 'Meryl Streep', 'Natalie Portman', 'Carey Mulligan', 10),
-(116, 'Painter known for "The Broken Column"', 'Georgia OKeeffe', 'Frida Kahlo', 'Tamara de Lempicka', 'Frida Kahlo', 10),
-(117, 'Singer known for "Sweet Child o Mine"', 'Stevie Nicks', 'Janis Joplin', 'Axl Rose', 'Stevie Nicks', 10),
-(118, 'Novel known for "The Talented Mr. Ripley"', 'The Catcher in the Rye', 'The Talented Mr. Ripley', 'The Great Gatsby', 'The Talented Mr. Ripley', 10),
-(119, 'Singer-songwriter known for "Under the Milky Way"', 'Bjork', 'Kate Bush', 'Sia', 'Kate Bush', 10),
-(120, 'Artist known for "The Dinner Party"', 'Georgia OKeeffe', 'Yayoi Kusama', 'Judy Chicago', 'Yayoi Kusama', 10),
-(121, 'Actress known for "Lady Bird"', 'Saoirse Ronan', 'Emma Stone', 'Florence Pugh', 'Saoirse Ronan', 10),
-(122, 'Painter known for "Self Portrait with Monkey"', 'Georgia OKeeffe', 'Frida Kahlo', 'Tamara de Lempicka', 'Frida Kahlo', 10),
-(123, 'Singer known for "Rabbit Heart (Raise It Up)"', 'Florence Welch', 'Lana Del Rey', 'Halsey', 'Florence Welch', 10),
-(124, 'Director of "Clueless"', 'Sofia Coppola', 'Amy Heckerling', 'Greta Gerwig', 'Amy Heckerling', 10),
-(125, 'Singer-songwriter known for "Running Up That Hill"', 'Bjork', 'Kate Bush', 'Sia', 'Kate Bush', 10),
-(126, 'Artist known for "No Woman No Cry"', 'Yayoi Kusama', 'Tracey Emin', 'Banksy', 'Tracey Emin', 10),
-(127, 'Actress known for "Kill Bill"', 'Uma Thurman', 'Meryl Streep', 'Cate Blanchett', 'Uma Thurman', 10),
-(128, 'Painter known for "The Love Embrace of the Universe"', 'Georgia OKeeffe', 'Frida Kahlo', 'Tamara de Lempicka', 'Frida Kahlo', 10),
-(129, 'Singer known for "Big Yellow Taxi"', 'Joni Mitchell', 'Carole King', 'Janis Joplin', 'Joni Mitchell', 10),
-(130, 'Author of Talent of Mr Ripley "', 'Patricia Highsmith', 'Susan Elderkin', 'Stanislaw Lew', 'Carol', 10);
-
--- the other tables
-INSERT INTO users (id_user, username, email, password, points, quizzes_resolved) VALUES
- (1, 'papapapa', 'pepe@gmail.com', '$2y$10$0p.yoFdkEUYUJNuNxlQwkud49Su7WE5IwpzBmFkQuxe43olXjUAf2', 1568, 5),
- (2, 'tutututu', 'user2@gmail.com', '$2y$10$5tjFcAS03s4GIOlwF0HwVu9txIob8xwgqSvJC4PkgSTq8EKLkIah2', 100, 4),
- (3, 'tatatata', 'user3@gmail.com', '$2y$10$53iKVgZjTPEHaY298ewgweIqeVs9dwKdR4RBBSY9EURFq1an5TYRi', 100, 3);
-
--- insert followrs
 INSERT INTO followers (id_user_follow, id_user_followed)
-VALUES
-(1, 2),
-(1, 3),
-(2, 1),
-(3, 1);
-
--- insert custom quizzes
-INSERT INTO custom_quizzes (id_quiz, quiz_name, n_questions, clock, time, fk_id_user)
-VALUES
-(1,"great books",  10, true, 30, 1),
-(2,"nobel prizes", 2, true, 22, 1),
-(3,"animals", 3, false, 10, 1);
-
--- insert likes
-INSERT INTO likes (fk_id_user, fk_id_quiz)
 VALUES
     (1, 2),
     (1, 3),
+    (2, 3),
+    (4, 1),
+    (5, 2);
+
+INSERT INTO quizzes (quiz_name, number_questions, clock, time, fk_id_user)
+VALUES
+    ('Quiz1', 10, TRUE, 10, 1),
+    ('Quiz2', 15, FALSE, NULL, 2),
+    ('Quiz3', 20, TRUE, 15, 4);
+
+INSERT INTO matches (type, fk_id_quiz)
+VALUES
+    ('single', 1),
+    ('multi', 2),
+    ('single', 3),
+    ('multi', 1),
+    ('single', 2);
+
+INSERT INTO custom_quizzes (id_quiz, fk_id_user)
+VALUES
+    (1, 1),
+    (2, 2),
+    (3, 4);
+
+
+INSERT INTO multiplayer_matches (id_match, winner, number_players)
+VALUES
+    (4, 'usuario4', 4),
+    (5, 'usuario2', 6);
+
+
+INSERT INTO singleplayer_matches (id_match, mode)
+VALUES
+    (1, 'quick'),
+    (3, 'mirror'),
+    (5, 'explod');
+
+INSERT INTO likes (fk_id_user, fk_id_quiz)
+VALUES
+    (1, 1),
+    (2, 2),
+    (3, 3);
+
+
+INSERT INTO users_plays_matches (id_user, id_match)
+VALUES
+    (1, 1),
     (2, 1),
-    (3, 1);
+    (3, 2),
+    (4, 3),
+    (5, 4);
+
+INSERT INTO questions (title, option_a, option_b, option_c, correct_option, points, type)
+VALUES
+('Director of "Lost in Translation"?', 'Sofia Coppola', 'Quentin Tarantino', 'Christopher Nolan', 'Sofia Coppola', 10, 'random'),
+('2014 Best Picture indie?', 'Moonlight', 'Boyhood', 'La La Land', 'Moonlight', 10, 'random'),
+('Greta Gerwig''s debut?', 'Lady Bird', 'Little Women', 'Frances Ha', 'Lady Bird', 10, 'random'),
+('Director of "The Hurt Locker"?', 'Kathryn Bigelow', 'Ava DuVernay', 'Patty Jenkins', 'Kathryn Bigelow', 10, 'random'),
+('Band behind "Vespertine"?', 'Björk', 'Radiohead', 'Arcade Fire', 'Björk', 10, 'random'),
+('"Lorde" real name?', 'Ella Marija Lani Yelich', 'Stefani Joanne Angelina', 'Robyn Rihanna Fenty', 'Ella Marija Lani Yelich', 10, 'random'),
+('The National''s lead vocalist?', 'Matt Berninger', 'Thom Yorke', 'Brandon Flowers', 'Matt Berninger', 10, 'random'),
+('Caroline Polachek most viral song?', 'Hurting My Feelings', 'Door', 'sunset', 'Hurting My Feelings', 10, 'random'),
+('1988 Summer Olympics host?', 'South Korea', 'United States', 'Australia', 'South Korea', 10, 'random'),
+( '2006 FIFA World Cup winner?', 'Italy', 'Brazil', 'Germany', 'Italy', 10, 'random'),
+( 'Sport associated with "slam dunk"?', 'Basketball', 'Football', 'Tennis', 'Basketball', 10, 'random'),
+( '"The Greatest" in boxing?', 'Muhammad Ali', 'Mike Tyson', 'Floyd Mayweather Jr.', 'Muhammad Ali', 10, 'random'),
+( 'First female UK Prime Minister?', 'Margaret Thatcher', 'Theresa May', 'Angela Merkel', 'Margaret Thatcher', 10, 'random'),
+( 'Year of the Berlin Wall fall?', '1989', '1991', '1987', '1989', 10, 'random'),
+( 'Longest US president term?', 'Franklin D. Roosevelt', 'George Washington', 'Abraham Lincoln', 'Franklin D. Roosevelt', 10, 'random'),
+( 'USSR leader during Cuban Missile Crisis?', 'Nikita Khrushchev', 'Leonid Brezhnev', 'Joseph Stalin', 'Nikita Khrushchev', 10, 'random'),
+( 'Current German Chancellor?', 'Olaf Scholz', 'Angela Merkel', 'Emmanuel Macron', 'Olaf Scholz', 10, 'random'),
+( 'First female Brazilian President?', 'Dilma Rousseff', 'Cristina Fernandez de Kirchner', 'Michelle Bachelet', 'Dilma Rousseff', 10, 'random'),
+( '"Libertarianism" political ideology?', 'Ltd government intervention', 'Strong government control', 'Totalitarianism', 'Ltd government intervention', 10, 'random'),
+( '44th US President?', 'Barack Obama', 'Donald Trump', 'George W. Bush', 'Barack Obama', 10, 'random'),
+( 'Actor: Tony Stark in MCU?', 'Robert Downey Jr.', 'Chris Hemsworth', 'Chris Evans', 'Robert Downey Jr.', 10, 'random'),
+( '"Queen of Pop"?', 'Madonna', 'Beyonce', 'Rihanna', 'Madonna', 10, 'random'),
+( 'Oscar winner for "La La Land"?', 'Emma Stone', 'Natalie Portman', 'Jennifer Lawrence', 'Emma Stone', 10, 'custom'),
+( 'Singer-songwriter of "25" (2015)?', 'Adele', 'Taylor Swift', 'Beyonce', 'Adele', 10, 'custom'),
+( 'Water chemical symbol?', 'H2O', 'CO2', 'NaCl', 'H2O', 10, 'custom'),
+( '"Red Planet"?', 'Mars', 'Venus', 'Jupiter', 'Mars', 10, 'custom'),
+( 'Australia capital city?', 'Canberra', 'Sydney', 'Melbourne', 'Canberra', 10, 'custom'),
+( '"To Kill a Mockingbird" author?', 'Harper Lee', 'John Steinbeck', 'Ernest Hemingway', 'Harper Lee', 10, 'custom'),
+('Painter known for "The Two Fridas"', 'Georgia OKeeffe', 'Frida Kahlo', 'Tamara de Lempicka', 'Tamara de Lempicka', 10, 'random'),
+('Singer known for "Rehab"', 'Adele', 'Amy Winehouse', 'Taylor Swift', 'Amy Winehouse', 10, 'random'),
+('Director of "The Hurt Locker"', 'Kathryn Bigelow', 'Ava DuVernay', 'Patty Jenkins', 'Kathryn Bigelow', 10, 'random'),
+('Singer-songwriter known for "Jolene"', 'Dolly Parton', 'Carole King', 'Joan Baez', 'Dolly Parton', 10, 'random'),
+('Artist known for "The Dinner Party"', 'Louise Bourgeois', 'Yayoi Kusama', 'Judy Chicago', 'Yayoi Kusama', 10, 'random'),
+('Actress known for "Breakfast at Tiffanys"', 'Audrey Hepburn', 'Marilyn Monroe', 'Grace Kelly', 'Audrey Hepburn', 10, 'random'),
+('Painter known for "Ophelia"', 'Georgia OKeeffe', 'Frida Kahlo', 'John Singer Sargent', 'Frida Kahlo', 10, 'random'),
+('Singer known for "Rolling in the Deep"', 'Adele', 'Amy Winehouse', 'Taylor Swift', 'Adele', 10, 'random'),
+('Director of "Carol"', 'Todd Haynes', 'J. Dacorneau', 'Francis Ford Coppola', 'Todd Haynes', 10, 'random'),
+('Singer-songwriter known for "Wuthering Heights"', 'Bjork', 'Kate Bush', 'Sia', 'Kate Bush', 10, 'random'),
+('Artist known for "My Bed"', 'Yayoi Kusama', 'Tracey Emin', 'Banksy', 'Tracey Emin', 10, 'random'),
+('Actress known for "Monster"', 'Charlize Theron', 'Meryl Streep', 'Angelina Jolie', 'Charlize Theron', 10, 'random'),
+('Painter known for "The Persistence of Memory"', 'Georgia OKeeffe', 'Frida Kahlo', 'Salvador Dali', 'Salvador Dali', 10, 'random'),
+('Singer known for "Big Yellow Taxi"', 'Joni Mitchell', 'Carole King', 'Janis Joplin', 'Joni Mitchell', 10, 'random'),
+('Director of "American Psycho"', 'Kathryn Bigelow', 'Sofia Coppola', 'Mary Harron', 'Mary Harron', 10, 'custom'),
+('Singer-songwriter known for "Pagan Poetry"', 'Bjork', 'Kate Bush', 'Sia', 'Bjork', 10, 'custom'),
+('Artist known for "Love Is What You Want"', 'Yayoi Kusama', 'Tracey Emin', 'Agnes Martin', 'Tracey Emin', 10, 'custom'),
+('Actress of "Mad Max: Fury Road"', 'Charlize Theron', 'Meryl Streep', 'Angelina Jolie', 'Charlize Theron', 10, 'custom'),
+('Artist of Self Portrait with Thorn Necklace and Hummingbird', 'Georgia OKeeffe', 'Frida Kahlo', 'Tamara de Lempicka', 'Frida Kahlo', 10, 'custom'),
+('Singer known for "River"', 'Joni Mitchell', 'Carole King', 'Janis Joplin', 'Joni Mitchell', 10, 'custom'),
+('Novel known for "Strangers on a Train"', 'Rebecca', 'Psycho', 'Strangers on a Train', 'Strangers on a Train', 10, 'custom'),
+('Singer-songwriter known for "Running Up That Hill"', 'Bjork', 'Kate Bush', 'Sia', 'Kate Bush', 10, 'custom'),
+('Artist known for "Spiral Jetty"', 'Yayoi Kusama', 'Tracey Emin', 'Agnes Martin', 'Agnes Martin', 10, 'custom'),
+('Actress known for "The Devil Wears Prada"', 'Anne Hathaway', 'Meryl Streep', 'Julia Roberts', 'Meryl Streep', 10, 'custom'),
+('Beach house bloom album year"', '2012', '2008', '2019', '2012', 10, 'custom'),
+('Singer known for "From the Choirgirl Hotel"', 'Tori Amos', 'PJ Harvey', 'Alanis Morissette', 'Tori Amos', 10, 'custom'),
+('Director of "American Psycho"', 'Kathryn Bigelow', 'Sofia Coppola', 'Mary Harron', 'Mary Harron', 10, 'custom'),
+('Singer-songwriter known for "Army of Me"', 'Bjork', 'Kate Bush', 'Sia', 'Bjork', 10, 'custom');
 
 -- insert custom questions of custom quizzes
-INSERT INTO custom_questions (id_question, title, option_a, option_b, option_c, correct_option, points, fk_id_quiz) VALUES
-(1,'Who wrote "To Kill a Mockingbird"?', 'Harper Lee', 'F. Scott Fitzgerald', 'John Steinbeck', 'Harper Lee', 10, 1),
-(2,'In which book does the character Holden Caulfield appear?', '1984', 'The Catcher in the Rye', 'The Great Gatsby', 'The Catcher in the Rye', 10, 1),
-(3,'Who wrote "Pride and Prejudice"?', 'Jane Austen', 'Charles Dickens', 'Emily Brontë', 'Jane Austen', 10, 1),
-(4,'What is the setting of "Moby-Dick"?', 'London', 'Paris', 'The sea', 'The sea', 10, 1),
-(5,'Who wrote "The Grapes of Wrath"?', 'Mark Twain', 'John Steinbeck', 'George Orwell', 'John Steinbeck', 10, 1),
-(6,'Which novel is set during the Russian Revolution?', 'Animal Farm', 'War and Peace', 'Crime and Punishment', 'Animal Farm', 10, 1),
-(7,'Who wrote "The Great Gatsby"?', 'F. Scott Fitzgerald', 'Ernest Hemingway', 'Mark Twain', 'F. Scott Fitzgerald', 10, 1),
-(8,'Which novel features the character Jay Gatsby?', 'Moby-Dick', 'The Great Gatsby', 'Wuthering Heights', 'The Great Gatsby', 10, 1),
-(9,'Who wrote "Wuthering Heights"?', 'Charlotte Brontë', 'Emily Brontë', 'Jane Austen', 'Emily Brontë', 10, 1),
-(10,'In which book does the character Elizabeth Bennet appear?', 'Pride and Prejudice', 'Sense and Sensibility', 'Emma', 'Pride and Prejudice', 10, 1);
+INSERT INTO custom_questions (id_question, fk_id_quiz)
+VALUES
+(56, 3),
+(23, 3),
+(24, 3),
+(25, 3),
+(26, 3),
+(27, 3),
+(28, 3);
 
-INSERT INTO custom_questions (id_question, title, option_a, option_b, option_c, correct_option, points, fk_id_quiz) VALUES
-(11, 'TNT inventor?', 'nobel', 'einstein', 'colombo', 'nobel', 10, 2),
-(12, 'TNT does?', 'kboom', 'pium', 'brumbrum', 'kboom', 10, 2);
 
-INSERT INTO custom_questions (id_question, title, option_a, option_b, option_c, correct_option, points, fk_id_quiz) VALUES
-(13,'What is the largest animal in the world?', 'Elephant', 'Blue whale', 'Gorilla', 'Blue whale', 10, 3),
-(14,'Which of these animals is a marine mammal?', 'Shark', 'Octopus', 'Dolphin', 'Dolphin', 10, 3),
-(15,'What is the fastest animal in the world?', 'Leopard', 'Cheetah', 'Eagle', 'Cheetah', 10, 3);
+
