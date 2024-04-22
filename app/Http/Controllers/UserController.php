dfb187073c1b34217474de35c2ecf45ecb40e17d
Patricio <pcifrey501@g.educaand.es>
2024-04-22 19:10:08 +0200

    laravel on dock-comp activated again as service

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,12 +3,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Exceptions\ActionFails;
 use App\Exceptions\AlreadyExist;
 use App\Exceptions\CustomNotFound;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Mockery\Exception;
 
