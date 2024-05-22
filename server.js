8ff24c416d812a3a7f37fc342e476b3cc771f35c
Patricio <pcifrey501@g.educaand.es>
2024-05-22 20:51:23 +0200

    implemented: handle room creation/connection

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const io = new Server(httpServer, {
     transports: ['websocket']
 })
 
+const rooms = {}
+
 const port = 3200;
 httpServer.listen(port, () => {
     console.log(`Socket.IO server running at http://localhost:${port}/`);
@@ -22,6 +24,17 @@ httpServer.listen(port, () => {
 io.on('connection', (socket) => {
 
     console.log(`User connected to socket:`, socket.id)
+
+    socket.on('joinRoom', (roomID) => {
+
+        socket.join(roomID)
+        console.log(`User ${socket.id} joined room ${roomID}`);
+
+        !rooms[roomID] ? rooms[roomID] = [socket.id] : rooms[roomID].push(socket.id)
+        console.log(rooms)
+        io.to(roomID).emit('userJoinedRoom', true, socket.id, rooms[roomID]);
+    });
+
 
     socket.on('disconnect', (reason) => {
         console.log('user disconnected from socket:', socket.id, '-Reason:', reason);
