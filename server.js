c653e28d74aa41dd6efb4298aefdfa75ce8fa9c6
Patricio <pcifrey501@g.educaand.es>
2024-05-25 14:40:58 +0200

    when user connect to the room html display their banner

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const io = new Server(httpServer, {
 })
 
 const rooms = {}
-
+const playersOnRoom = []
 const port = 3200;
 httpServer.listen(port, () => {
     console.log(`Socket.IO server running at http://localhost:${port}/`);
@@ -25,13 +25,27 @@ io.on('connection', (socket) => {
 
     console.log(`User connected to socket:`, socket.id)
 
-    const playersOnRoom = {}
-
     socket.on('joinRoom', (data) => {
         console.log(data)
         socket.join(data.roomID)
-        if (!rooms[data.roomID]) rooms[data.roomID] = [socket.id]
-        else if (rooms[data.roomID].length<4) rooms[data.roomID].push(socket.id)
+        if (!rooms[data.roomID]){
+
+            rooms[data.roomID] = [{
+                username: data.username,
+                userSocket: socket.id
+            }]
+            playersOnRoom.push(data.username)
+
+        }
+        else if (rooms[data.roomID].length<4){
+            rooms[data.roomID].push({
+                username: data.username,
+                userSocket: socket.id
+            })
+            playersOnRoom.push(data.username)
+
+
+        }
         else  io.to(data.roomID).emit('fullRoom', true)
 
         console.log(rooms)
